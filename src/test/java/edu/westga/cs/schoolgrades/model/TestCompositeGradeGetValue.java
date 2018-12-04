466fd30330044c6e5665b84e8c285e98ab463e32
James Luke Johnson <jjohn144@my.westga.edu>
2018-12-04 07:37:47 -0600

    Rewrote CompositeGradeGetValue to use verify mock instead of assert

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeGetValue.java
@@ -1,7 +1,7 @@
 package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -19,18 +19,20 @@ class TestCompositeGradeGetValue {
 		when(mockGrade1.getValue()).thenReturn(20.0);
 		Grade mockGrade2 = mock(Grade.class);
 		when(mockGrade2.getValue()).thenReturn(30.0);
+		
 		List<Grade> grades = new ArrayList<Grade>();
 		grades.add(mockGrade0);
 		grades.add(mockGrade1);
 		grades.add(mockGrade2);
+		
 		GradeCalculationStrategy mockStrategy = mock(GradeCalculationStrategy.class);
-		when(mockStrategy.calculate(grades)).thenReturn(60.0);
 		
 		CompositeGrade composite = new CompositeGrade(mockStrategy);
 		composite.add(mockGrade0);
 		composite.add(mockGrade1);
 		composite.add(mockGrade2);
-		assertEquals(60, composite.getValue(), 0.01);
+		composite.getValue();
+		verify(mockStrategy).calculate(grades);
 	}
 
 }
